d3a8b10f99e2389781186bc0e8dd0e97f655533c
sebascasasg <sebascasas.developer@gmail.com>
2021-06-12 12:33:58 -0500

    Created and styled components for learning resources

---
+++ learning-resources-app/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <stored-resources :resources="storedResources"></stored-resources>
+  </div>
+</template>
+
+<script>
+import StoredResources from '@/components/learning-resources/StoredResources.vue';
+
+export default {
+  components: {
+    StoredResources
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'vue-documentation',
+          title: 'Official Vue Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://v3.vuejs.org/'
+        },
+        {
+          id: 'frontend-masters',
+          title: 'Frontend Masters',
+          description: 'Modern front-end engineering courses',
+          link: 'https://frontendmasters.com/'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ learning-resources-app/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,64 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h2>{{ title }}</h2>
+        <button>Delete</button>
+      </header>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    },
+
+    description: {
+      type: String
+    },
+
+    link: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
